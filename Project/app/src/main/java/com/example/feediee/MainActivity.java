0349d5c3b96966c35d2e0873f46bbd0168b17bf1
khyathimsit <khyathi17@msitprogram.net>
2019-03-19 19:13:17 +0530

    Activity-14

---
+++ Project/app/src/main/java/com/example/feediee/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.feediee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText phoneNumber, password;
+    private Button login;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        phoneNumber = (EditText) findViewById(R.id.login_number_input);
+        password = (EditText) findViewById(R.id.login_password_input);
+        login = (Button) findViewById(R.id.login_btn);
+    }
+}
